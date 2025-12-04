d7ee708d88b730a66ef00a4e2cd0390d5fbf5c6e
Anna Zhevlakova <azevlakova12@gmail.com>
2025-12-04 13:16:11 +0400

    start

---
+++ src/main/java/com/example/ImageConversion/ImageConversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImageConversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageConversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImageConversion/ImageConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImageConversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
